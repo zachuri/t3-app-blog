bc67df6dcd12c091525d43455ca8616cba7fe140
zachuri <punsalang.zachary@gmail.com>
2022-08-02 23:24:29 -0700

    added comment about (MyApp) with trpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -52,9 +52,11 @@ export default withTRPC({
 
 	// false because we want to be able to see every
 	//  single request our client is making
-  
+
 	ssr: false, //play with this setting that gives the best performance and UX
 
 	// if true -> client will make request on the server
 	//  wont be able to see those request in the network tab
-});
+})(MyApp);
+
+// adding (MyApp) for our app to execute with TRPC
