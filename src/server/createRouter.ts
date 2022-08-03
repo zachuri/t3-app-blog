bb653447031147b381ebb95567619116fb07f851
zachuri <punsalang.zachary@gmail.com>
2022-08-02 23:39:38 -0700

    added type Context in createRouter from createContext

--- src/server/createRouter.ts
+++ src/server/createRouter.ts
@@ -1,8 +1,11 @@
 import { router } from "@trpc/server"; // helper function -> create router -> will be fully typed for us
 import superjson from "superjson";
+import { Context } from "./createContext";
 
 export function createRouter() {
-	// todo add context to generic
-	return router().transformer(superjson); //helps to not need to call .transformer on every single router we create
+	// Context type to for each router to have a type of res & req from createContext
+	return router<Context>().transformer(superjson);
+
+	//helps to not need to call .transformer on every single router we create
 	// Ex. we have app.router.ts, post.router.ts, user.router.ts
 }
