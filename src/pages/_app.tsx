8c662ee91128b6b63a1ff4f7a1c8720bc7ac4b0d
zachuri <punsalang.zachary@gmail.com>
2022-08-05 00:52:30 -0700

    made a constants url to use in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { loggerLink } from "@trpc/client/links/loggerLink";
 import { httpBatchLink } from "@trpc/client/links/httpBatchLink";
 import superjson from "superjson";
 import { AppRouter } from "../server/route/app.router";
+import { url } from "../utils/constants";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
@@ -12,15 +13,11 @@ function MyApp({ Component, pageProps }: AppProps) {
 
 export default withTRPC<AppRouter>({
 	config({ ctx }) {
-		const url = process.env.NEXT_PUBLIC_VERCEL_URL
-			? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
-			: "http://localhost:3000/api/trpc";
-
 		const links = [
 			loggerLink(),
 			httpBatchLink({
 				maxBatchSize: 10,
-				url,
+				url: url,
 			}),
 		];
 
