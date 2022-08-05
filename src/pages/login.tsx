6fa9919e473148fd02361ddfad7a3f1a1641e1e1
zachuri <punsalang.zachary@gmail.com>
2022-08-05 01:01:31 -0700

    completed login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { CreateUserInput } from "../schema/user.schema";
 import { trpc } from "../utils/trpc";
@@ -10,40 +11,45 @@ function LoginPage() {
 	const { handleSubmit, register } = useForm<CreateUserInput>();
 	//note register value is something created by useForm
 
+	const [success, setSuccess] = useState(false);
+
 	const router = useRouter();
 
 	// Most cases you will use mutate
-	// const { mutate, mutateAsync, error } = trpc.useMutation(
-	// 	[
-	// 		//trpc ->  ./utils -> createRouter -> app.router -> user.router
-	// 		"users.register-user",
-	// 	],
-	// 	{
-	// 		onSuccess: () => {
-	// 			router.push("/login");
-	// 		},
-	// 	}
-	// );
+	const { mutate, mutateAsync, error } = trpc.useMutation(
+		[
+			//trpc ->  ./utils -> createRouter -> app.router -> user.router
+			"users.request-otp",
+		],
+		{
+			onSuccess: () => {
+				setSuccess(true);
+			},
+		}
+	);
 
 	// onSubmit make sure the values are good types of CreateUserInput
 	function onSubmit(values: CreateUserInput) {
-		// mutate(values);
+		mutate(values);
 	}
 
 	return (
 		<>
 			<form onSubmit={handleSubmit(onSubmit)}>
-				{/* {error && error.message} */}
+				{error && error.message}
+				{success && <p>Check your email</p>}
+
+				<h1>Login</h1>
+
+				<input
+					type="email"
+					placeholder="jane.doe@example.com"
+					{...register("email")}
+				/>
+
+				<button type="submit">Login</button>
 			</form>
-			<h1>Login</h1>
 
-			<input
-				type="email"
-				placeholder="jane.doe@example.com"
-				{...register("email")}
-			/>
-
-			<button type="submit">Login</button>
 			<Link href="/register">Register</Link>
 		</>
 	);
