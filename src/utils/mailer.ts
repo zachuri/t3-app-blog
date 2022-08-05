caa79b1fc84935e39d5ffa910d8d02e7013d634a
zachuri <punsalang.zachary@gmail.com>
2022-08-05 01:11:28 -0700

    added comments in util/mailer about how it works a bit

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -21,12 +21,15 @@ export async function sendLoginEmail({
 		},
 	});
 
+	// This is an example of what would get from an email
+	// you change the host later
+	// in terminal you will get a link to see the email
 	const info = await transporter.sendMail({
 		from: '"Jane Doe" <j.doe@example.com',
 		to: email,
 		subject: "Login to your account",
 		// use # instead of ? because you don't want the token to be save in the browswer history
-		html: `Login by clicking here <a href="${url}/login#token=${token}">`,
+		html: `Login by clicking <a href="${url}/login#token=${token}"> HERE <a/>`,
 	});
 
 	console.log(`Preview URL: ${nodemailer.getTestMessageUrl(info)}`);
