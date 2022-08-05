a02bcc2210ab10e7795ad0e29f4603ac7917c16b
zachuri <punsalang.zachary@gmail.com>
2022-08-05 01:13:07 -0700

    added comments in constants (has our urls)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,7 @@
-export const url = process.env.NEXT_PUBLIC_VERCEL_URL
-	? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
-	: "http://localhost:3000/api/trpc";
+// base url
+export const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+	? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+	: "http://localhost:3000/";
+
+// url to be able to call our api
+export const url = `${baseUrl}/api/trpc`;
