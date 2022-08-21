a9bd29e83d669abc8db0bb190ca24b6814fe5f81
zachuri <punsalang.zachary@gmail.com>
2022-08-21 16:03:28 -0700

    added post router to our app.router.ts to merge

--- src/server/route/app.router.ts
+++ src/server/route/app.router.ts
@@ -1,7 +1,11 @@
 import { createRouter } from "../createRouter";
+import { postRouter } from "./post.router";
 import { userRouter } from "./user.router";
 
 // App Router is the main connection between all your other routers or apis
-export const appRouter = createRouter().merge("users.", userRouter);
+export const appRouter = createRouter()
+  .merge("users.", userRouter)
+  .merge("posts.", postRouter);
+
 
 export type AppRouter = typeof appRouter;
