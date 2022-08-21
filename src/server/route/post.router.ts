4700938e74e6b8c68623b6763d1442dace95c4ef
zachuri <punsalang.zachary@gmail.com>
2022-08-21 16:19:55 -0700

    post.router.ts setup

--- src/server/route/post.router.ts
+++ src/server/route/post.router.ts
@@ -1,6 +1,15 @@
+import { createPostSchema, getSinglePostSchema } from "../../schema/post.schema";
 import { createRouter } from "../createRouter";
 
 export const postRouter = createRouter()
-	.mutation("create-post", {})
-	.query("posts", {})
-  .query("single-post", {});
+	.mutation("create-post", {
+		input: createPostSchema,
+		async resolve() {},
+	})
+	.query("posts", {
+		resolve() {},
+	})
+	.query("single-post", { 
+    input: getSinglePostSchema
+		resolve() {},
+   });
