056cc972989fb5408d51b1637a904e0eff3b1998
zachuri <punsalang.zachary@gmail.com>
2022-08-21 16:27:36 -0700

    finished querys for creating/ viewing posts

--- src/server/route/post.router.ts
+++ src/server/route/post.router.ts
@@ -1,15 +1,49 @@
-import { createPostSchema, getSinglePostSchema } from "../../schema/post.schema";
+import { createNextApiHandler } from "@trpc/server/adapters/next";
+import {
+	createPostSchema,
+	getSinglePostSchema,
+} from "../../schema/post.schema";
 import { createRouter } from "../createRouter";
+import * as trpc from "@trpc/server";
 
 export const postRouter = createRouter()
 	.mutation("create-post", {
 		input: createPostSchema,
-		async resolve() {},
+		async resolve({ ctx, input }) {
+			if (!ctx.user) {
+				return new trpc.TRPCError({
+					code: "FORBIDDEN",
+					message: "Cannot create a post while logged out",
+				});
+			}
+
+			// create post in prisma data base
+			const post = await ctx.prisma.post.create({
+				data: {
+					...input,
+					user: {
+						connect: {
+							id: ctx.user.id,
+						},
+					},
+				},
+			});
+
+			return post;
+		},
 	})
 	.query("posts", {
-		resolve() {},
+		resolve({ ctx }) {
+			return ctx.prisma.post.findMany();
+		},
 	})
-	.query("single-post", { 
-    input: getSinglePostSchema
-		resolve() {},
-   });
+	.query("single-post", {
+		input: getSinglePostSchema,
+		resolve({ ctx, input }) {
+			return ctx.prisma.post.findUnique({
+				where: {
+					id: input.postId,
+				},
+			});
+		},
+	});
